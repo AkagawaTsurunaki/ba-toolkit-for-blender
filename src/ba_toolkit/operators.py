8aeef493b361a15f50d18ea62e3a82e5fbf3cec1
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2026-08-21 19:03:16 +0800

    fix: Fix the orientation of the imported bones

--- src/ba_toolkit/operators.py
+++ src/ba_toolkit/operators.py
@@ -15,19 +15,26 @@ class AnimatorImporter(bpy.types.Operator):
         subtype='DIR_PATH',
         description="Select the folder containing the animator files (FBX and textures) exported from Assets Studio.\n"
         'For example, D:/AssetsStudioOutput/Animator/Aris_Original'
-    )
-
+    )  # type: ignore
 
     def execute(self, context):
         dir = Path(str(self.directory))
         assert dir.exists()
         anim = get_original_animator(dir=dir)
-        
-        bpy.ops.import_scene.fbx(filepath=str(anim.fbx_path), use_image_search=True)
+
+        # Fixed the importing issue by setting `use_image_search` to True.
+        # Fixed the bone orientation issue by setting `automatic_bone_orientation` to True.
+        # Maybe the GUI of Blender doesn't have these options in the import dialog?
+        # This `global_scale` is set to 200 because the original model is too small in Blender.
+        # And this scale is also good for Minecraft based animation creation.
+        bpy.ops.import_scene.fbx(filepath=str(anim.fbx_path),
+                                 use_image_search=True,
+                                 automatic_bone_orientation=True,
+                                 global_scale=200)
         print(anim)
         return {'FINISHED'}
 
     def invoke(self, context, event):
         self.directory = ""
-        context.window_manager.fileselect_add(self)
+        context.window_manager.fileselect_add(self)  # type: ignore
         return {'RUNNING_MODAL'}
