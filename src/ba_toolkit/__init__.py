9c19dc0f3e68195fd207c6fa0837a0f02fbf1188
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2026-08-23 22:57:29 +0800

    feat: Bind mouth controller to selected mesh

--- src/ba_toolkit/__init__.py
+++ src/ba_toolkit/__init__.py
@@ -1,6 +1,5 @@
-from .operators import model_importer, mouth_separator, texture_fixer
+from .operators import model_importer, mouth_separator, texture_fixer, mouth_binder
 import bpy
-
 
 bl_info = {
     "name": "BA Toolkit for Blender",
@@ -28,21 +27,26 @@ class BAToolkitPanel(bpy.types.Panel):
             row = layout.row()
             if row is not None:
                 row.operator(model_importer.ModelImporter.bl_idname,
-                             icon=model_importer.ModelImporter.bl_icon)  # type: ignore
+                             icon=model_importer.ModelImporter.bl_icon)
             row = layout.row()
             if row is not None:
                 row.operator(texture_fixer.TextureFixer.bl_idname,
-                             icon=texture_fixer.TextureFixer.bl_icon)  # type: ignore
+                             icon=texture_fixer.TextureFixer.bl_icon)
             row = layout.row()
             if row is not None:
                 row.operator(mouth_separator.MouthSeparator.bl_idname,
-                             icon=mouth_separator.MouthSeparator.bl_icon)  # type: ignore
+                             icon=mouth_separator.MouthSeparator.bl_icon)
+            row = layout.row()
+            if row is not None:
+                row.operator(mouth_binder.MouthBinder.bl_idname,
+                             icon=mouth_binder.MouthBinder.bl_icon)
 
 
 def register():
     bpy.utils.register_class(model_importer.ModelImporter)
     bpy.utils.register_class(texture_fixer.TextureFixer)
     bpy.utils.register_class(mouth_separator.MouthSeparator)
+    bpy.utils.register_class(mouth_binder.MouthBinder)
     bpy.utils.register_class(BAToolkitPanel)
 
 
@@ -50,6 +54,7 @@ def unregister():
     bpy.utils.unregister_class(model_importer.ModelImporter)
     bpy.utils.unregister_class(texture_fixer.TextureFixer)
     bpy.utils.unregister_class(mouth_separator.MouthSeparator)
+    bpy.utils.unregister_class(mouth_binder.MouthBinder)
     bpy.utils.unregister_class(BAToolkitPanel)
 
 
---
+++ src/ba_toolkit/operators/mouth_binder.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+import bpy
+
+
+class MouthBinder(bpy.types.Operator):
+    bl_idname = "ba_toolkit_for_blender.mouth_binder"
+    bl_label = "Bind Mouth to Selected"
+    bl_options = {'REGISTER', 'UNDO'}
+    bl_icon = 'LINK_BLEND'
+    bl_description = "Bind mouth controller to selected mesh.\n" \
+                     "Instructions:\n" \
+                     "1. Select the mouth mesh (e.g., Aris_Origin_Body_Mouth).\n" \
+                     '2. Click "Bind Mouth to Selected". A mouth controller will be added to the scene.\n' \
+                     "3. Switch to Pose Mode.\n" \
+                     "4. Click the little selection box on the surface of the 'Mouth Sprite Sheet'.\n" \
+                     "5. Press 'G' to move it around and pick the mouth shape you like.\n" \
+                     'Thanks to BlackMaLou for sharing the "Mouth Sprite Sheet"'
+
+    def execute(self, context):
+        mouth = context.active_object
+        if not mouth or mouth.type != 'MESH':
+            self.report({'ERROR'}, "No mesh selected. Please select a mouth mesh!")
+            return {'CANCELLED'}
+
+        template_path = Path(__file__).parent.parent.joinpath('assets/Mouth_BlackMaLou.blend')
+
+        print(template_path)
+        with bpy.data.libraries.load(str(template_path), link=False) as (src, dst):
+            dst.collections = ["Mouth Controller"]
+
+        imported = dst.collections[0]
+        imported.name = f"{mouth.name} Controller"
+        context.scene.collection.children.link(imported)
+
+        # Find the mouth template in the collection
+        template = next(
+            (o for o in imported.objects if o.type == 'MESH' and 'template' in o.name.lower()),
+            None
+        )
+        if not template:
+            self.report({'ERROR'}, 'Mouth Template is None? Did you modify assets/Mouth_BlackMaLou.blend?')
+            return {'CANCELLED'}
+
+        # Delete all old materials!
+        while mouth.data.materials:
+            mouth.data.materials.pop(index=0)
+
+        for mat in template.data.materials:
+            mouth.data.materials.append(mat)
+
+        self.report({'INFO'}, f"Bind {template.name} to {mouth.name} successfully!")
+        return {'FINISHED'}
