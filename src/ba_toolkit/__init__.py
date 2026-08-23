1bedd64389d1ec8fb7e6f55271a9bab8cc2d5806
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2026-08-23 23:33:42 +0800

    refactor: Modified category

--- src/ba_toolkit/__init__.py
+++ src/ba_toolkit/__init__.py
@@ -7,8 +7,11 @@ bl_info = {
     "version": (0, 1, 0),
     "blender": (4, 5, 6),
     "location": "View3D > Sidebar > BA Toolkit",
-    "description": "...",
-    "category": "Material",
+    "description": "This toolkit is a helpful Blender add‑on for creating 3D animations using Blue Archive models. "
+                   "It assists with model import, attempts to fix texture issues, "
+                   "and automatically separates the mouth mesh then binds it to a mouth controller, "
+                   "enabling faster animation setup.",
+    "category": "Rigging",
 }
 
 
